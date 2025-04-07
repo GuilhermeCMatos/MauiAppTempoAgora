27571f0df7c91dc5545e921c0b90f3ed5c860efb
231052 <231052@CPD02-M17>
2025-04-07 10:58:39 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiAppTempoAgora.Models;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade) 
+        {
+        }
+    }
+}
